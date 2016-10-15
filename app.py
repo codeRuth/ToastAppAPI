db17fc6961414e081316f5c8c78bcfdabaa2bb84
coderuth <coderuth@outlook.com>
2016-10-15 15:52:12 +0530

    minor changes

--- app.py
+++ app.py
@@ -1,32 +1,19 @@
 #!bin/python
 from flask import Flask, jsonify
-from getrecipe import get_recipe
+from get_recipe import getRecipe
 
 app = Flask(__name__)
 
-l = [
-    {'a': 1, 'b': 2},
-    {'c': 3, 'd': 4}
-]
+
+@app.route('/recipe/<int:id>', methods=['GET'])
+def get_recipe(recipe_id):
+    return recipe_id
 
 
-@app.route('/api/recipe/tasks', methods=['GET'])
-def getTasks():
-    return jsonify({'tasks': l})
-
-
-@app.route('/api/<string:ing>', methods=['GET'])
-def get_task(ing):
-    # task = [task for task in tasks if task['id'] == task_id]
-    # if len(task) == 0:
-    #     abort(404)
-    # ing = 'shredded chicken,'
-    # get_r =
-    obj = get_recipe(ing, 1)
-    return obj.return_recipe()
-    # print get_r.return_recipe.content
-    # return jsonify(l)
-
+@app.route('/recipes/<string:ing>', methods=['GET'])
+def get_recipes(ing):
+    obj = getRecipe(ing, 1)
+    return jsonify(obj.return_recipe())
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000, debug=True)
+    app.run(host='127.0.0.1', port=8000, debug=True)
---
+++ get_recipe.py
@@ -0,0 +1,42 @@
+import requests
+import json
+
+API_KEY = 'a7117cb7dc7f8b768ec323b949e752dd'
+
+
+class getRecipe(object):
+    def __init__(self, ingredient, page):
+        self.list = ingredient
+        self.page = page
+
+    def return_recipe(self):
+        response = []
+        num = 0
+        page = requests.get('http://food2fork.com/api/search?key=' + API_KEY +
+                            '&q=' + self.list +
+                            '&page=' + str(self.page))
+        parsed = json.loads(page.text)
+        count = parsed['count']
+
+        for i in range(0, count):
+            if is_valid(parsed['recipes'][i]['publisher']):
+                temp = {
+                    'title': parsed['recipes'][i]['title'],
+                    'id': parsed['recipes'][i]['recipe_id']
+                }
+                response.append(temp)
+        return response
+
+
+def is_valid(inp):
+    valid_publishers = ['All Recipes', 'Back to Her Roots', 'BBC Food', 'BBC Good Food', 'Bon Appetit',
+                        'Bunky Cooks', 'Chow', 'Closet Cooking', 'Cookin Canuck', 'Epicurious', 'Food Republic',
+                        'Framed Cooks', 'Healthy Delicious', 'Jamie Oliver', "Lisa's Kitchen"]
+    for i in range(len(valid_publishers)):
+        if inp == valid_publishers[i]:
+            return True
+    return False
+
+if __name__ == '__main__':
+    obj = getRecipe("shredded chicken", 1)
+    print obj.return_recipe()
